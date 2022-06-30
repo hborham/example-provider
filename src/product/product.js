8378f7ff39f667ce40af013eae97f83d37774ad5
hazem.borham <hazem@tendosystems.com>
2022-06-30 08:29:51 -0700

    add tapable feature

--- src/product/product.js
+++ src/product/product.js
@@ -1,9 +1,10 @@
 class Product {
-    constructor(id, type, name, version) {
+    constructor(id, type, name, version, tappable) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
+        this.tappable = tappable
     }
 }
 
